1760006506e24bab6c479ea6a50b789ee488cdc5
NDT <namanthacker965@gmail.com>
2019-07-20 16:16:41 +0530

    minor changes

--- Selection.c
+++ Selection.c
@@ -6,31 +6,31 @@ int main()
 {
     int total_ele;
     printf("Enter number of elements:");
-    scanf("%d",&total_ele);
+    scanf("%d", &total_ele);
     int arr[total_ele];
-    int i,j,k=0,min;
-    for(i=0;i<total_ele;i++)
-        scanf("%d",&arr[i]);
-    for(i=0;i<total_ele;i++)
+    int i, j, k = 0, min;
+    for(i = 0; i < total_ele; i++)
+        scanf("%d", &arr[i]);
+    for(i = 0; i < total_ele; i++)
     {
-        k=-1;
-        min=arr[i];
-        for(j=i+1;j<total_ele;j++)
+        k = -1;
+        min = arr[i];
+        for(j = i+1; j < total_ele; j++)
         {
 
-            if(min>arr[j])
+            if(min > arr[j])
             {
-                min=arr[j];
-                k=j;
+                min = arr[j];
+                k = j;
             }
         }
-        if(k!=-1)
+        if(k != -1)
         {
-        arr[k]=arr[i];
-        arr[i]=min;
+            arr[k] = arr[i];
+            arr[i] = min;
         }
 
     }
-    for(j=0;j<total_ele;j++)
-        printf("%d\t",arr[j]);
+    for(j = 0;j < total_ele; j++)
+        printf("%d\t", arr[j]);
 }
